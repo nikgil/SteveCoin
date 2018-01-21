e2ea3a2b3be7026f8ba7543c159f1fe92c9cc094
Nik Gil <nikmanG@outlook.ie>
2018-01-21 05:07:30 +0000

    Add new tests for version.

--- src/test/java/io/github/nikmang/stevecoin/crypto/BlockchainTest.java
+++ src/test/java/io/github/nikmang/stevecoin/crypto/BlockchainTest.java
@@ -20,18 +20,25 @@ public class BlockchainTest {
 
     @Test
     public void addValidBlock() {
-        Block b = new Block(chain.getChain().size(), chain.getChain().getLast().getHash(), "Valid Block");
+        Block b = chain.generateNextBlock("test block");
+
         Assert.assertTrue(chain.addBlockToChain(b));
     }
 
     @Test
     public void addInvalidBlocks() {
+        Block valid = chain.generateNextBlock("Test Block"); //Using this for invalid block creation
+
         //Index is off
-        Block test = new Block(chain.getChain().size()+1, chain.getChain().getLast().getHash(), "Bad Index");
+        Block test = new Block(valid.getIndex()+1, valid.getTimestamp(), valid.getPrevHash(), valid.getData(), valid.getDifficulty(), valid.getNonce());
         Assert.assertFalse(chain.addBlockToChain(test));
 
         //Invalid hash
-        test = new Block(chain.getChain().size(), chain.getChain().getLast().getHash() + "abc", "Bad Hash");
+        test = new Block(valid.getIndex()+1, valid.getTimestamp(), "1"+valid.getPrevHash(), valid.getData(), valid.getDifficulty(), valid.getNonce());
+        Assert.assertFalse(chain.addBlockToChain(test));
+
+        //Invalid timestamp
+        test = new Block(valid.getIndex()+1, chain.getChain().getLast().getTimestamp(), "1"+valid.getPrevHash(), valid.getData(), valid.getDifficulty(), valid.getNonce());
         Assert.assertFalse(chain.addBlockToChain(test));
     }
 
@@ -39,19 +46,29 @@ public class BlockchainTest {
     public void failedForceAdd() {
         Deque<Block> list = chain.getChain();
 
-        list.add(new Block(chain.getChain().size(), chain.getChain().getLast().getHash() + "abc", "Bad Hash 2"));
+        list.add(chain.getChain().getFirst());
 
         Assert.assertFalse(list.getLast().equals(chain.getChain().getLast()));
     }
 
     @Test
+    public void testDifficulty() {
+        while(chain.getChain().size() < 100) {
+            chain.addBlockToChain(chain.generateNextBlock("Block #" + chain.getChain().size()));
+        }
+
+        System.out.println("Latest difficulty: " + chain.getChain().getLast().getDifficulty());
+        Assert.assertEquals(chain.getDifficulty(), chain.getChain().getLast().getDifficulty());
+    }
+
+    @Test
     public void replaceSuccessfulChain() {
         Deque<Block> list = chain.getChain();
-        list.add(new Block(chain.getChain().size(), chain.getChain().getLast().getHash(), "Valid Block 2"));
+        list.add(chain.generateNextBlock("Test Block 2"));
 
         Assert.assertTrue(chain.replaceChain(list));
 
-        list.add(new Block(chain.getChain().size()+1, chain.getChain().getLast().getHash(), "Bad Index 2"));
+        list.add(list.getFirst()); //technically should fault on the timestamp or index
         Assert.assertFalse(chain.replaceChain(list));
 
         Assert.assertEquals(list.size()-1, chain.getChain().size()); //chain should not have the last block
