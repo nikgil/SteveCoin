e8335fa784856f2c9f6c8e5396b3fa6e13ef014b
Nik Gil <nikmanG@outlook.ie>
2018-01-24 03:56:04 +0000

    Change data from string to transactions.

--- src/main/java/io/github/nikmang/stevecoin/crypto/Block.java
+++ src/main/java/io/github/nikmang/stevecoin/crypto/Block.java
@@ -1,20 +1,26 @@
 package io.github.nikmang.stevecoin.crypto;
 
 import com.google.common.hash.Hashing;
+import io.github.nikmang.stevecoin.crypto.transactions.Transaction;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Individual Block in a blockchain.
  */
 public class Block {
 
-    public static final Block GENESIS_BLOCK = new Block(0, System.currentTimeMillis(), "", "Hello World", 0, 0);
+    public static final Block GENESIS_BLOCK = new Block(0, System.currentTimeMillis(), "", 0, 0);
 
     private int index;
-    private String hash, prevHash; //TODO: delete prevHash if unused
+    private String hash, prevHash;
+    private String merkleRoot;
+
+    private List<Transaction> data;
+
     private long timestamp;
-    private String data;
 
     private int difficulty;
     private long nonce;
@@ -25,27 +31,31 @@ public class Block {
      * @param index      The index of the block.
      * @param timestamp  The timestamp of creation of the block.
      * @param prevHash   Previous block's hash. Done in SHA256.
-     * @param data       The data incorporated within the block.
      * @param difficulty The difficulty to mine set block.
      * @param nonce      non-reusable number that is used to prevent replay attacks.
      */
-    public Block(int index, long timestamp, String prevHash, String data, int difficulty, long nonce) {
+    public Block(int index, long timestamp, String prevHash, int difficulty, long nonce) {
         assert prevHash != null && data != null;
 
         this.prevHash = prevHash;
         this.index = index;
         this.timestamp = timestamp;
-        this.data = data;
 
         this.difficulty = difficulty;
         this.nonce = nonce;
 
         this.hash = genHash();
+        this.data = new ArrayList<>();
+        this.merkleRoot = "";
     }
 
     //Creates the hash for the block
     private String genHash() {
-        return Hashing.sha256().hashString(index + prevHash + timestamp + data + difficulty + nonce, StandardCharsets.UTF_8).toString();
+        return Hashing.sha256().hashString(index + prevHash + timestamp + merkleRoot + difficulty + nonce, StandardCharsets.UTF_8).toString();
+    }
+
+    public void setMerkleRoot(String root) {
+        this.merkleRoot = root;
     }
 
     // Getters //
@@ -66,7 +76,7 @@ public class Block {
         return timestamp;
     }
 
-    public String getData() {
+    public List<Transaction> getData() {
         return data;
     }
 
@@ -76,6 +86,10 @@ public class Block {
 
     public long getNonce() {
         return nonce;
+    }
+
+    public String getMerkleRoot() {
+        return merkleRoot;
     }
 
     @Override
